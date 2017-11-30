eafe529b6aebd9048fe5b9723927617edf740f41
Benedict Aluan <benedict@boost.co.nz>
2017-12-01 09:52:48 +1300

    Add Team model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020020418) do
+ActiveRecord::Schema.define(version: 20171130205010) do
 
   create_table "players", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
     t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/models/team_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Team, type: :model do
+  describe 'validations' do
+  end
+
+  describe 'relationships' do
+  end
+end
