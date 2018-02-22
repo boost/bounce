43c81b091686b335da9e014928ea66ae23af8ead
Ben <hapiben@users.noreply.github.com>
2018-02-23 12:41:09 +1300

    Remove commented out codes (#11)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  # provider :slack, ENV['SLACK_KEY'], ENV['SLACK_SECRET'],
-           # scope: 'team:read,users:read,identify,bot,incoming-webhook,commands,chat:write:user,chat:write:bot', team: 'boostnz'
   provider :slack, ENV['SLACK_KEY'], ENV['SLACK_SECRET'], scope: 'identity.basic', name: :sign_in_with_slack, team: 'boostnz'
   provider :slack, ENV['SLACK_KEY'], ENV['SLACK_SECRET'], scope: 'identify,bot'
 end
--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -1,7 +1,3 @@
-# Slack.configure do |config|
-#   config.token = ENV['SLACK_API_TOKEN_APP']
-# end
-
 Slack.configure do |config|
   config.token = ENV['SLACK_BOT_TOKEN']
 end
