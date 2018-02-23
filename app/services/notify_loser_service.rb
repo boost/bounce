934e137f863e3f14053dc4a0bb3406f4b2e4fded
Benedict Aluan <benedict@boost.co.nz>
2018-02-23 14:40:45 +1300

    Text change

--- app/services/notify_loser_service.rb
+++ app/services/notify_loser_service.rb
@@ -27,7 +27,7 @@ class NotifyLoserService
     options = {
       channel: "##{ENV['SLACK_CHANNEL']}",
       as_user: true,
-      text: "#{losers}, team #{winners} said they beat you at Ping Pong!",
+      text: "#{losers}, #{winners} said they beat you at Ping Pong!",
       attachments: attachments.to_json
     }
 
