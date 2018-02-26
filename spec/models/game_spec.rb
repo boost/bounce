ce9fd72b8baf9183a412ed43f0742ff379559dac
Benedict Aluan <benedict@boost.co.nz>
2018-02-27 07:46:39 +1300

    Improve Game spec

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -5,6 +5,11 @@ RSpec.describe Game, type: :model do
   let(:team) { create(:team, players: players) }
   let(:game) { create(:game, winner: team, loser: team) }
 
+  describe 'validations' do
+    it { validate_presence_of :winner }
+    it { validate_presence_of :loser }
+  end
+
   describe 'relationships' do
     it { belong_to :winner }
     it { belong_to :loser }
@@ -13,6 +18,20 @@ RSpec.describe Game, type: :model do
   describe 'callbacks' do
     it 'sets confirmed to false' do
       expect(game.confirmed?).to eq false
+    end
+  end
+
+  describe 'scopes' do
+    context 'recent_games' do
+      it 'returns confirmed' do
+        game.confirmed = true
+        game.save
+        expect(described_class.recent_games).to eq [game]
+      end
+
+      it 'returns unconfirmed' do
+        expect(described_class.recent_games(confirmed: false)).to eq [game]
+      end
     end
   end
 end
