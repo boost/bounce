cdf8e6082a532a9c03b296b461e6e3f8abfd0044
Benedict Aluan <benedict@boost.co.nz>
2018-04-20 11:41:33 +1200

    Install Vue.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,7 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import Demo from 'demo'
+
+console.log(Demo)
 console.log('Hello World from Webpacker')
